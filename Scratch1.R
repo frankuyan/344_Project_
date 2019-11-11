5d82cadf80c675494a1921815ed900c038f467c8
Frank <fyan@alumni.ubc.ca>
2019-11-10 16:26:55 -0800

    1.0

--- Scratch1.R
+++ Scratch1.R
@@ -1,7 +1,25 @@
 #Basic Analysis on the Scratch information of the Phone Usage
 
+SurveyData <- Phone.and.Social.Media.Usage.Survey
 
-surveyData <- Phone.and.Social.Media.Usage.Survey
+#surveyData <- read.csv(Phone.and.Social.Media.Usage.Survey)
+
+a <- toapply(SurveyData$V2, SurveyData$V3, length)
+#Checking over the data for "Gender" and "Year Level" 
 
 
-surveyData
+#select those states with more than 20 records:
+a <- tapply(agpop$farms92,agpop$state,length) 
+b <- a[a > 20]; b <- names(b)
+agpop <- agpop[agpop$state %in% b,]
+
+
+
+
+GenderInfo <- (Phone.and.Social.Media.Usage.Survey$V2)
+
+summary(GenderInfo)
+
+
+
+YearLevel <- c(Phone.and.Social.Media.Usage.Survey$V3)
